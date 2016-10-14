60b6a26b5fd6ccb3e2f0f266a06d3b8a968dd5e2
lingxiao007a@gmail.com <lingxiao007a@gmail.com>
2016-10-14 18:28:01 +0800

    Hello map, GPS and overlay.

--- app/src/main/java/com/example/lx/justmap/MapActivity.java
+++ app/src/main/java/com/example/lx/justmap/MapActivity.java
@@ -2,22 +2,95 @@ package com.example.lx.justmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 
 public class MapActivity extends AppCompatActivity {
     MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+
+    // 定位相关
+    public LocationClient mLocationClient;
+    public MyLocationListener mLocationListener;
+
+    private boolean isFirstIn = true;
+    private double mLatitude;
+    private double mLongtitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+        initView();
+
+        // 定位
+        initLocation();
+
+    }
+
+    private void initLocation(){
+        mLocationClient = new LocationClient(this);     //声明LocationClient类
+        mLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener( mLocationListener );    //注册监听函数
+
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        mLocationClient.setLocOption(option);
+    }
+
+    private void initView() {
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -35,5 +108,71 @@ public class MapActivity extends AppCompatActivity {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_map_common :
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.id_map_site :
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.id_map_traffic :
+                if (mBaiduMap.isTrafficEnabled()) {
+                    mBaiduMap.setTrafficEnabled(false);
+                    item.setTitle(R.string.menu_map_traffic_off);
+                } else {
+                    mBaiduMap.setTrafficEnabled(true);
+                    item.setTitle(R.string.menu_map_traffic_on);
+                }
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build();
+
+            mBaiduMap.setMyLocationData(data);
+
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            if (isFirstIn) {
+                LatLng latLng = new LatLng(mLatitude, mLongtitude);
+                MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+
+                //定义Maker坐标点
+                LatLng point = new LatLng(mLatitude + 0.00001, mLongtitude + 0.00001);
+                //构建Marker图标
+                BitmapDescriptor bitmap = BitmapDescriptorFactory
+                        .fromResource(R.drawable.icon_friends);
+                //构建MarkerOption，用于在地图上添加Marker
+                OverlayOptions option = new MarkerOptions()
+                        .position(point)
+                        .icon(bitmap);
+                //在地图上添加Marker，并显示
+                mBaiduMap.addOverlay(option);
+                mBaiduMap.animateMapStatus(msu);
+
+                isFirstIn = false;
+            }
+        }
     }
 }
